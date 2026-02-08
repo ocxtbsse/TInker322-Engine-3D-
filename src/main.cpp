1975002ff6beed5e9324f8b73989e5e07ec08cea
ocxtbsse <ocxtbsse@gmail.com>
2026-02-08 18:31:16 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,9 @@
 #include <GLM/gtc/matrix_transform.hpp>
 #include <GLM/gtc/type_ptr.hpp>
 #include "DeltaTime.hpp"
+#include <vector>
 
-// вершинный шейдер
+
 const char* vSource = R"(
     #version 330 core
     layout (location = 0) in vec3 aPos;
@@ -79,31 +80,26 @@ float verticesCubeSigma[] = {
      0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  0.0f,  0.0f,  1.0f,
     -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  0.0f,  0.0f,  1.0f,
 
-    // ЗАДНЯЯ (Z-)
     -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,  0.0f,  0.0f, -1.0f,
      0.5f, -0.5f, -0.5f,  1.0f, 0.0f,  0.0f,  0.0f, -1.0f,
      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  0.0f,  0.0f, -1.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,  0.0f,  0.0f, -1.0f,
 
-    // ЛЕВАЯ (X-)
     -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, -1.0f,  0.0f,  0.0f,
     -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, -1.0f,  0.0f,  0.0f,
     -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, -1.0f,  0.0f,  0.0f,
     -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, -1.0f,  0.0f,  0.0f,
 
-    // ПРАВАЯ (X+)
      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  1.0f,  0.0f,  0.0f,
      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  1.0f,  0.0f,  0.0f,
      0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  1.0f,  0.0f,  0.0f,
      0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  1.0f,  0.0f,  0.0f,
 
-    // НИЖНЯЯ (Y-)
     -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,  0.0f, -1.0f,  0.0f,
      0.5f, -0.5f, -0.5f,  1.0f, 1.0f,  0.0f, -1.0f,  0.0f,
      0.5f, -0.5f,  0.5f,  1.0f, 0.0f,  0.0f, -1.0f,  0.0f,
     -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  0.0f, -1.0f,  0.0f,
 
-    // ВЕРХНЯЯ (Y+)
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,  0.0f,  1.0f,  0.0f,
      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  0.0f,  1.0f,  0.0f,
      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  0.0f,  1.0f,  0.0f,
@@ -115,13 +111,55 @@ float verticesCubeSigma[] = {
 
 
 unsigned int indices[] = {
-    0, 1, 2, 2, 3, 0,       // Передняя
-    4, 5, 6, 6, 7, 4,       // Задняя
-    8, 9, 10, 10, 11, 8,    // Левая
-    12, 13, 14, 14, 15, 12, // Правая
-    16, 17, 18, 18, 19, 16, // Нижняя
-    20, 21, 22, 22, 23, 20  // Верхняя
+    0, 1, 2, 2, 3, 0,    
+    4, 5, 6, 6, 7, 4,      
+    8, 9, 10, 10, 11, 8,   
+    12, 13, 14, 14, 15, 12, 
+    16, 17, 18, 18, 19, 16, 
+    20, 21, 22, 22, 23, 20  
 };
+
+
+struct BoxCollider {
+    glm::vec3* pos;
+    glm::vec3 size;
+    bool isStatic;
+};
+
+
+
+void resolveCollision(const BoxCollider& wall, BoxCollider& player, bool& grounded) {
+    glm::vec3 delta = *player.pos - *wall.pos;
+
+    glm::vec3 halfSizeWall = wall.size * 0.5f;
+    glm::vec3 halfSizePlayer = player.size * 0.5f;
+
+    float overlapX = (halfSizeWall.x + halfSizePlayer.x) - glm::abs(delta.x);
+    float overlapY = (halfSizeWall.y + halfSizePlayer.y) - glm::abs(delta.y);
+    float overlapZ = (halfSizeWall.z + halfSizePlayer.z) - glm::abs(delta.z);
+
+    if (overlapX > 0 && overlapY > 0 && overlapZ > 0) {
+        
+        if (overlapX < overlapY && overlapX < overlapZ) {
+            player.pos->x += (delta.x > 0) ? overlapX : -overlapX;
+        } 
+        else if (overlapY < overlapX && overlapY < overlapZ) {
+            player.pos->y += (delta.y > 0) ? overlapY : -overlapY;
+            if (delta.y > 0) {
+                player.pos->y += overlapY;                
+                grounded = true;
+            }
+                
+        } 
+        else {
+            player.pos->z += (delta.z > 0) ? overlapZ : -overlapZ;
+        }
+    }
+}
+
+
+
+
 
 
 
@@ -184,13 +222,27 @@ int main(){
 
 
     glm::vec3 cubePosition = glm::vec3(3.0f,0.0f,-3.0f);
-    glm::vec3 cubePosition2 = glm::vec3(0.0f,0.0f,0.0f);
+    glm::vec3 cubePosition2 = glm::vec3(0.0f,2.0f,0.0f);
 
-    float cubeScale = 3.0f;
+
+    glm::vec3 cubeScale = glm::vec3(100.0f,1.0f,100.0f);
+    glm::vec3 cube2Scale = glm::vec3(1.0f,4.0f,1.0f);
+    float gravity = 15.0f;
+
+
+
     float speed = 10.0f;
 
     double mouseX, mouseY;
     static double lastX = 400.0, lastY = 300.0;
+
+
+
+
+    std::vector<BoxCollider> worldColliders;
+    worldColliders.push_back({ &cubePosition, cubeScale, true }); 
+    BoxCollider playerColl = { &cubePosition2, cube2Scale, false };
+
 
 
     glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f, 0.7f);    
@@ -199,7 +251,8 @@ int main(){
 
     float yaw   = -90.0f; 
     float pitch = 0.0f;
-
+    glm::vec3 cs = glm::vec3(0.0f, 2.0f, 0.0f); 
+    float velY = 0.0f;
     glfwSetInputMode(w1.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     while (!glfwWindowShouldClose(w1.window)) {
         Time::update();
@@ -209,12 +262,15 @@ int main(){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
+        
+        cameraPos = cubePosition2 + cs; 
+
 
         prog.use();
         glUniform1i(glGetUniformLocation(prog.ID, "ourTexture"), 0); 
-        glm::vec3 lightPos(1.2f, 2.0f, 2.0f); 
+        glm::vec3 lightPos(1.2f, 10.0f, 2.0f); 
         prog.setVec3("lightPos", lightPos);
-        prog.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+        prog.setVec3("lightColor", glm::vec3(0.4f, 0.4f, 0.4f));
 
 
 
@@ -249,41 +305,43 @@ int main(){
         float cameraSpeed = speed * Time::deltaTime;
 
 
-        if (glfwGetKey(w1.window, GLFW_KEY_W) == GLFW_PRESS){
-            cameraPos += cameraFront * cameraSpeed;
+        glm::vec3 forward = glm::normalize(glm::vec3(cameraFront.x, 0.0f, cameraFront.z));
+        glm::vec3 right = glm::normalize(glm::cross(forward, cameraUp));
+        
+        
+        if (glfwGetKey(w1.window, GLFW_KEY_W) == GLFW_PRESS) cubePosition2 += forward * cameraSpeed;
+        if (glfwGetKey(w1.window, GLFW_KEY_S) == GLFW_PRESS) cubePosition2 -= forward * cameraSpeed;
+        if (glfwGetKey(w1.window, GLFW_KEY_A) == GLFW_PRESS) cubePosition2 -= right * cameraSpeed;
+        if (glfwGetKey(w1.window, GLFW_KEY_D) == GLFW_PRESS) cubePosition2 += right * cameraSpeed;
+        
+
+        
+        velY -= gravity * Time::deltaTime; 
+        cubePosition2.y += velY * Time::deltaTime;
+
+        bool isGrounded = false; 
+        for (auto& wall : worldColliders) {
+            resolveCollision(wall, playerColl, isGrounded);
         }
-        if (glfwGetKey(w1.window, GLFW_KEY_S) == GLFW_PRESS){
-            cameraPos -= cameraFront * cameraSpeed;
-        }
-        if (glfwGetKey(w1.window, GLFW_KEY_A) == GLFW_PRESS){
-            cameraPos -= cameraRight * cameraSpeed;
+
+        if (isGrounded) {
+            if (velY < 0) velY = 0; 
+
+            if (glfwGetKey(w1.window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+                velY = 7.0f;
+            }
         }
         
-        if (glfwGetKey(w1.window, GLFW_KEY_D) == GLFW_PRESS){
-            cameraPos += cameraRight * cameraSpeed;
-        }
+        
 
-
-
-        //передвижение куба
-        if (glfwGetKey(w1.window, GLFW_KEY_LEFT) == GLFW_PRESS){
-            cubePosition.x -= speed * Time::deltaTime;
-        }
-        if (glfwGetKey(w1.window, GLFW_KEY_RIGHT) == GLFW_PRESS){
-            cubePosition.x += speed * Time::deltaTime;
-        }
-
-        if (glfwGetKey(w1.window, GLFW_KEY_UP) == GLFW_PRESS){
-            cubePosition.z -= speed * Time::deltaTime;
-        }
-        if (glfwGetKey(w1.window, GLFW_KEY_DOWN) == GLFW_PRESS){
-            cubePosition.z += speed * Time::deltaTime;
-        }
+        
+        
+        
 
         glm::mat4 projection = glm::perspective(glm::radians(70.0f), 800.0f/600.0f, 0.1f, 100.0f);
         glm::mat4 view = glm::lookAt(cameraPos,cameraPos + cameraFront,cameraUp);
-
-
+        
+        
         prog.setMat4("view",view);
         prog.setMat4("projection",projection);
         
@@ -294,16 +352,12 @@ int main(){
 
 
 
-        glm::mat4 model2 = glm::mat4(1.0f);
-        model2 = glm::translate(model2, cubePosition2);
-        prog.setMat4("model",model2);
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
         
 
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, cubePosition);
+        model = glm::scale(model, cubeScale);
         prog.setMat4("model", model);
-        glBindTexture(GL_TEXTURE_2D, texture1); 
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0); 
         
         
