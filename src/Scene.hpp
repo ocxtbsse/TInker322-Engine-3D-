59ec41a47bf1aea2eab6567817baa54983daaad4
ocxtbsse <ocxtbsse@gmail.com>
2026-02-15 00:35:37 +0300

    Create Scene.hpp

---
+++ src/Scene.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "OBS.hpp"
+#include <vector>
+
+
+extern std::vector<GameObject> sceneObjects;
+extern std::vector<Camera> sceneCamers;
+
+void pushBackObject(GameObject PtrGMO);//лень менять название
+void pushBackCamera(Camera PtrCam);//лень менять назв 
