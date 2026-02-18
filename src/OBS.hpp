ca98851485b50f3cb7f979df2bf438e7cb83f1bd
ocxtbsse <ocxtbsse@gmail.com>
2026-02-18 22:56:11 +0300

    Update OBS.hpp

--- src/OBS.hpp
+++ src/OBS.hpp
@@ -5,20 +5,28 @@
 #include <GLM/gtc/matrix_transform.hpp>
 #include <GLM/gtc/type_ptr.hpp>
 #include <vector>
+#include "Matrix.h"
 
 struct GameObject{
-    glm::vec3 position;
-    glm::vec3 scale;
-    glm::vec3 rotation;
 
-    glm::mat4 getModelMatrix() {
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, position);
-        model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1, 0, 0));
-        model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0, 1, 0));
-        model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0, 0, 1));
-        model = glm::scale(model, scale);
-        
+
+
+
+
+    VectorS position;
+    VectorS scale;
+    VectorS rotate;
+
+
+    char* tag;
+    bool visible = true;
+
+
+    Matrix getModelMatrix4x4(){
+        Matrix model;
+        model.set_transform(position.vec3[X],position.vec3[Y],position.vec3[Z]);
+        model.rotateMat4x4(rotate.vec3[X] * DEGRAD,rotate.vec3[Y] * DEGRAD,rotate.vec3[Z] * DEGRAD);
+        model.scaleMat4x4(scale.vec3[X],scale.vec3[Y],scale.vec3[Z]);
         return model;
     }
 
@@ -35,19 +43,24 @@ struct GameObject{
         char* textureName;
         unsigned int textureID;
     } material;
-
-
-    struct Collider{//скоро
-
-    } collider;
 };
 
 
+
+
+
+
+
+
+
 struct Camera{
-    
-    glm::vec3 position;    
-    glm::vec3 front;
-    glm::vec3 up;
+    VectorS position;
+    VectorS front;
+    VectorS up;
+
+
+
+
     
 
     
@@ -59,14 +72,32 @@ struct Camera{
     float FOV = 70.0f;
     float nearPlane = 0.1f;
     float farPlane = 1000.0f;
-    glm::mat4 getCameraProjcetionMatrix() {
+
+
+    Matrix getProectionMat4x4(){
         float aspectRatio = static_cast<float>(screenWidth) / static_cast<float>(screenHeight);
-        glm::mat4 projection = glm::perspective(glm::radians(FOV), aspectRatio, nearPlane, farPlane);
+        Matrix projection;
+        projection.perspective(FOV,aspectRatio,nearPlane,farPlane);
         return projection;
     }
 
-    glm::mat4 getCameraViewMatrix(){
-        glm::mat4 view = glm::lookAt(position,position + front,up);;
+
+
+
+
+
+    Matrix getCameraViewMat4x4(){
+        Matrix view;
+
+
+        VectorS target;
+        target.vec3[0] = this->position.vec3[0] + this->front.vec3[0];
+        target.vec3[1] = this->position.vec3[1] + this->front.vec3[1];
+        target.vec3[2] = this->position.vec3[2] + this->front.vec3[2];
+
+        view.lookAt(this->position, target, this->up);
+
         return view;
+
     }
 };
